3545926f5392277a52083949726c21514865f291
randyaajr <randyaajrsp@gmail.com>
2021-03-05 20:57:48 +0000

    View created for portfolio page

--- run.py
+++ run.py
@@ -10,6 +10,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
